983439a98c5d7320e14d72462378674a350954dc
Haitham abu lamdi <hayhtam14@gmail.com>
2023-04-11 11:07:26 +0200

    edit project structure

--- src/controller/store/candy.js
+++ src/controller/store/candy.js
@@ -1,29 +1,34 @@
-const {getCandy,addCandy, deleteCandy} = require('../../database/queries/candy');
-const { join }= require('path');
+const {
+  getCandy,
+  addCandy,
+  deleteCandy,
+} = require("../../database/queries/candy");
+const { join } = require("path");
 const getCandies = (req, res) => {
-    getCandy()
-      .then(result => {
-        res.json(result.rows);
-      })
-      .catch(err => console.log("err:", err));
-  };
+  getCandy()
+    .then((result) => {
+      res.json(result.rows);
+    })
+    .catch((err) => console.log("err:", err));
+};
 
-  const addNewCandy = (req,res)=>{
-    const candyInfo = req.body;
-    addCandy(candyInfo)
+const addNewCandy = (req, res) => {
+  const candyInfo = req.body;
+  addCandy(candyInfo)
     .then(() => res.redirect("/"))
-    .catch(err => console.log("err:", err));
-  }
+    .catch((err) => console.log("err:", err));
+};
 
-  const deleteCandies = (req,res)=>{
-    deleteCandy(req.params.id)
-    .then(() => res.redirect("/") )//
-    .catch(err => console.log("err:", err));
-  };
+const deleteCandies = (req, res) => {
+  deleteCandy(req.params.id)
+    .then(() => res.redirect("/")) //
+    .catch((err) => console.log("err:", err));
+};
 
+const getAddForm = (req, res) => {
+  res.sendFile(
+    join(__dirname, "..", "..", "..", "public", "html", "addCAndyForm.html")
+  );
+};
 
-  const getAddForm=(req,res)=>{
-    res.sendFile(join(__dirname,'..','..','..','public','addCAndyForm.html'));
-  }
-  
-  module.exports = {getCandies,addNewCandy,deleteCandies,getAddForm};
+module.exports = { getCandies, addNewCandy, deleteCandies, getAddForm };
