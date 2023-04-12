dd7fa57168d7cc1783d1d510b41178a11a9c20a3
Batrawi <batrawi2000forever@gmail.com>
2023-04-13 00:27:00 +0300

    login done

--- src/controller/auth/index.js
+++ src/controller/auth/index.js
@@ -2,4 +2,4 @@ const {checkUserAuth,checkUserRole} = require('./loginController');
 const signup = require('./signupController')
 const logoutController = require('./logoutController')
 
-module.exports={checkUserAuth,checkUserRole,signup};
+module.exports={checkUserAuth,checkUserRole,signup,logoutController};
