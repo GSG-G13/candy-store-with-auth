4dc0bff8e12273c2c551c7f65cc99a1e87f81f35
abeerAyad <abeerayyad2000@gmail.com>
2023-04-12 23:41:34 +0200

    save code

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,35 +1,36 @@
-const add_button = document.querySelector(".addCandyButton");
-const addCategoryButton = document.querySelector(".addCategoryButton");
-const addFlavorButton = document.querySelector(".addFlavorButton");
+const add_button = document.querySelector('.addCandyButton')
+const addCategoryButton = document.querySelector('.addCategoryButton')
+const addFlavorButton = document.querySelector('.addFlavorButton')
 
-fetchFunction("/candies").then((res) => createCard(res));
 
-fetchFunction("/categories").then((res) => createList("category", res));
+fetchFunction('/candies').then((res) => createCard(res))
 
-fetchFunction("/flavors").then((res) => createList("flavor", res));
+fetchFunction('/categories').then((res) => createList('category', res))
 
-add_button.addEventListener("click", () => {
-  window.location.href = "/addCandyForm";
-});
-addCategoryButton.addEventListener("click", () => {
-  window.location.href = "/addCategory";
-});
-addFlavorButton.addEventListener("click", () => {
-  window.location.href = "/addFlavor";
-});
+fetchFunction('/flavors').then((res) => createList('flavor', res))
 
-category_id.addEventListener("change", () => {
-  flavor_id.value = "";
+add_button.addEventListener('click', () => {
+  window.location.href = '/addCandyForm'
+})
+addCategoryButton.addEventListener('click', () => {
+  window.location.href = '/addCategory'
+})
+addFlavorButton.addEventListener('click', () => {
+  window.location.href = '/addFlavor'
+})
+
+category_id.addEventListener('change', () => {
+  flavor_id.value = ''
   if (!category_id.value) {
-    fetchFunction("/candies").then((res) => createCard(res));
+    fetchFunction('/candies').then((res) => createCard(res))
   }
-  fetchFunction(`/oneCat/${category_id.value}`).then((res) => createCard(res));
-});
+  fetchFunction(`/oneCat/${category_id.value}`).then((res) => createCard(res))
+})
 
-flavor_id.addEventListener("change", () => {
-  category_id.value = "";
+flavor_id.addEventListener('change', () => {
+  category_id.value = ''
   if (!flavor_id.value) {
-    fetchFunction("/candies").then((res) => createCard(res));
+    fetchFunction('/candies').then((res) => createCard(res))
   }
-  fetchFunction(`/oneFlav/${flavor_id.value}`).then((res) => createCard(res));
-});
+  fetchFunction(`/oneFlav/${flavor_id.value}`).then((res) => createCard(res))
+})
